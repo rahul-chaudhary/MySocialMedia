b0cdbc22250356cb2a05c41c00e469776bd56a44
unknown <bossrc64@outlook.com>
2024-03-19 15:00:29 +0530

    Added ActivityMainBinding in the MainActivity.kt

--- app/src/main/java/com/example/mysocialmedia/MainActivity.kt
+++ app/src/main/java/com/example/mysocialmedia/MainActivity.kt
@@ -5,15 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mysocialmedia.databinding.ActivityMainBinding
+import com.example.mysocialmedia.databinding.ActivitySignInBinding
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.SignInClient
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var mbinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mbinding.root)
 
 
     }
