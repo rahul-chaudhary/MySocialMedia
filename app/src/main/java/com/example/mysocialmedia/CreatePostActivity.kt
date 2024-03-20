0766a9a7b88f85cc4aad12ef48c7337c054f0e00
unknown <bossrc64@outlook.com>
2024-03-20 12:13:53 +0530

    Added addPost function in setOnClickListener on postbtn

--- app/src/main/java/com/example/mysocialmedia/CreatePostActivity.kt
+++ app/src/main/java/com/example/mysocialmedia/CreatePostActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mysocialmedia.daos.PostDao
 import com.example.mysocialmedia.databinding.ActivityCreatePostBinding
 
 class CreatePostActivity : AppCompatActivity() {
     private lateinit var mbinding: ActivityCreatePostBinding
+    private lateinit var postDao: PostDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,7 +20,7 @@ class CreatePostActivity : AppCompatActivity() {
         mbinding.postbtn.setOnClickListener {
             val input = mbinding.postInput.text.toString().trim()
             if(input.isNotEmpty()) {
-
+                postDao.addPost(input)
             }
         }
 
