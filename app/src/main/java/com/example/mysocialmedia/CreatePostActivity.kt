9b1d1a70b5e5f37e3b4348cb6ae1e3fe11373b75
unknown <bossrc64@outlook.com>
2024-03-20 11:10:45 +0530

    Declared mbinding variable, initialized it and changed setContentView parameter to mbinding.root

--- app/src/main/java/com/example/mysocialmedia/CreatePostActivity.kt
+++ app/src/main/java/com/example/mysocialmedia/CreatePostActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mysocialmedia.databinding.ActivityCreatePostBinding
 
 class CreatePostActivity : AppCompatActivity() {
+    private lateinit var mbinding: ActivityCreatePostBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_create_post)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        mbinding = ActivityCreatePostBinding.inflate(layoutInflater)
+        setContentView(mbinding.root)
+
     }
 }
