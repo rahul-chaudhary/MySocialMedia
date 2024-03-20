b0ba810af92f8f3b6b7ec2ece432ef434e8f99e8
unknown <bossrc64@outlook.com>
2024-03-20 12:11:25 +0530

    Added more functionalities to addPost function in PostDao

--- app/src/main/java/com/example/mysocialmedia/daos/PostDao.kt
+++ app/src/main/java/com/example/mysocialmedia/daos/PostDao.kt
@@ -1,14 +1,31 @@
 package com.example.mysocialmedia.daos
 
+import com.example.mysocialmedia.models.Post
+import com.example.mysocialmedia.models.User
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class PostDao {
-    val db = FirebaseFirestore.getInstance()
-    val postCollections = db.collection("posts")
-    val auth = Firebase.auth
+    private val db = FirebaseFirestore.getInstance()
+    private val postCollections = db.collection("posts")
+    private val auth = Firebase.auth
 
-    fun addPost(text: String) {}
+    @OptIn(DelicateCoroutinesApi::class)
+    fun addPost(text: String) {
+        val currentUserId = auth.currentUser!!.uid
+        GlobalScope.launch {
+            val userDao = UserDao()
+            val user = userDao.getUserById(currentUserId).await().toObject(User::class.java)!!
+
+            val currentTime = System.currentTimeMillis()
+            val post = Post(text, user, currentTime)
+            postCollections.document().set(post)
+        }
+    }
 
 }
