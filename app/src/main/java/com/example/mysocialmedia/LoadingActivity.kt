cfdf6f33c7e70d80dc4dfc1c1fadf00666928976
unknown <bossrc64@outlook.com>
2024-03-27 15:13:51 +0530

    Add: Lottie xml code

--- app/src/main/java/com/example/mysocialmedia/LoadingActivity.kt
+++ app/src/main/java/com/example/mysocialmedia/LoadingActivity.kt
@@ -5,16 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.mysocialmedia.databinding.ActivityLoadingBinding
 
 class LoadingActivity : AppCompatActivity() {
+    private lateinit var mbinding: ActivityLoadingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_loading)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        mbinding = ActivityLoadingBinding.inflate(layoutInflater)
+        setContentView(mbinding.root)
+
+
         }
     }
-}
