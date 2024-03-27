14d50cb3da9ebabc92a6f9eae6e8da04901d8a1a
unknown <bossrc64@outlook.com>
2024-03-27 17:44:07 +0530

    Add: New lottie json file

--- app/src/main/java/com/example/mysocialmedia/LoadingActivity.kt
+++ app/src/main/java/com/example/mysocialmedia/LoadingActivity.kt
@@ -1,12 +1,14 @@
 package com.example.mysocialmedia
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil.setContentView
 import com.example.mysocialmedia.databinding.ActivityLoadingBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LoadingActivity : AppCompatActivity() {
     private lateinit var mbinding: ActivityLoadingBinding
@@ -15,6 +17,18 @@ class LoadingActivity : AppCompatActivity() {
         mbinding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(mbinding.root)
 
-
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(5000) // 5000 milliseconds = 5 seconds
+            startMainActivity()
         }
+
     }
+    private fun startMainActivity() {
+        val intent = Intent(this@LoadingActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish() // Finish the LoadingActivity so it's not accessible via back button
+    }
+
+
+}
+
